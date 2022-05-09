a859ea49c59968e78283460bd637f0cc46e58d07
caathyn <caathynguyen@gmail.com>
2022-05-09 11:13:52 -0700

    solved buildingobjects

---
+++ buildingObjects.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Only change code below this line
+  name: "Cloudy",
+  legs: 4,
+  tails: 2,
+  friends: ["Brownie", "Charlie", "Muffin"]
+
+  // Only change code above this line
+};
