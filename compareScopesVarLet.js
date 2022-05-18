38303e4e388c9a9aca8ea4a0b621b3c7e25f7db5
caathyn <caathynguyen@gmail.com>
2022-05-17 19:15:38 -0700

    solved compareScopesVarLet

--- compareScopesVarLet.js
+++ compareScopesVarLet.js
@@ -1,13 +1,4 @@
 function checkScope() {
-  var i = 'function scope';
-  if (true) {
-    i = 'block scope';
-    console.log('Block scope i is: ', i);
-  }
-  console.log('Function scope i is: ', i);
-  return i;
-}
-function checkScope() {
   let i = 'function scope';
   if (i === 'block scope') {
     i = 'block scope';
