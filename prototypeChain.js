f320df61be6bcd6f91fb69397209b4d9ab30c5ac
caathyn <caathynguyen@gmail.com>
2022-05-22 21:22:46 -0700

    solved prototypechain

--- prototypeChain.js
+++ prototypeChain.js
@@ -7,4 +7,4 @@ let beagle = new Dog("Snoopy");
 Dog.prototype.isPrototypeOf(beagle);  // yields true
 
 // Fix the code below so that it evaluates to true
-???.isPrototypeOf(Dog.prototype);
+Object.prototype.isPrototypeOf(Dog.prototype);
