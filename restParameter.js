9305ead42450b08ce2436fcdb00d1c1bc334c461
caathyn <caathynguyen@gmail.com>
2022-05-23 20:01:54 -0700

    solved restparameter

--- restParameter.js
+++ restParameter.js
@@ -1,4 +1,5 @@
-const sum = (x, y, z) => {
-  const args = [x, y, z];
-  return args.reduce((a, b) => a + b, 0);
+const sum = (...args) => {
+  return args.reduce((a,b) => a + b, 0);
 }
+
+console.log(sum(1, 2, 3));
