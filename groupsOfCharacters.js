47ada7a7a1ef141da811d0c63b46d01eff82207a
caathyn <caathynguyen@gmail.com>
2022-06-10 12:19:42 -0700

    solved groupsofcharacters

---
+++ groupsOfCharacters.js
@@ -0,0 +1,6 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/g; // Change this line
+let result = true; // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+
+myRegex.test(myString);
