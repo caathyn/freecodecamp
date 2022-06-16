e19f8ece4c99fd41499d1f72e63af2cefe59c62b
caathyn <caathynguyen@gmail.com>
2022-06-16 12:59:13 -0700

    solved titlecase

--- titleCase.js
+++ titleCase.js
@@ -1,5 +1,9 @@
 function titleCase(str) {
-  return str;
+  let sentence = str.toLowerCase().split(' ');
+  for (let i=0; i < sentence.length; i++) {
+    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+  }
+  return sentence.join(' ');
 }
 
-titleCase("I'm a little tea pot");
+console.log(titleCase("I'm a little tea pot"));
