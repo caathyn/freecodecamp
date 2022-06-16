81d2eeb51d14e1f08e6a1b77153043626b644909
caathyn <caathynguyen@gmail.com>
2022-06-16 14:43:31 -0700

    solved sliceandsplice

--- sliceAndSplice.js
+++ sliceAndSplice.js
@@ -1,5 +1,10 @@
 function frankenSplice(arr1, arr2, n) {
-  return arr2;
+  let array = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    array.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return array;
 }
 
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
