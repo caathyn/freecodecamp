50dcd225096e8c826e8691ad3704b113a2e6e5d0
caathyn <caathynguyen@gmail.com>
2022-06-17 11:22:01 -0700

    solved falsybouncer

--- falsyBouncer.js
+++ falsyBouncer.js
@@ -1,9 +1,17 @@
+//solution 1
 function bouncer(arr) {
   let newArray = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i]) newArray.push(arr[i]);
   }
   return newArray;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+//solution 2
+function bouncer(arr) {
+  return arr.filter(Boolean);
 }
 
 bouncer([7, "ate", "", false, 9]);
