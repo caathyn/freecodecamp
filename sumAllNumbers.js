9cfc1ba5348e997d52f57dc2a27d368f98f6400f
caathyn <caathynguyen@gmail.com>
2022-06-20 21:27:59 -0700

    solved sumallnumbers

--- sumAllNumbers.js
+++ sumAllNumbers.js
@@ -1,5 +1,11 @@
 function sumAll(arr) {
-  return 1;
+  let min = Math.min(arr[0], arr[1]);
+  let max = Math.max(arr[0], arr[1]);
+  let sumBetween = 0;
+  for (let i = min; i <= max; i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
 }
 
-sumAll([1, 4]);
+console.log(sumAll([1, 4]));
