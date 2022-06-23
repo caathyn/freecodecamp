d3b863111b75728a9cfc95f89a5aa85c3a9bb6f6
caathyn <caathynguyen@gmail.com>
2022-06-23 12:37:19 -0700

    solved joinmethod

--- sortMethod.js
+++ sortMethod.js
@@ -2,8 +2,10 @@ function alphabeticalOrder(arr) {
   // Only change code below this line
 
   return arr.sort(function (a, b) {
-    return a === b ? 0 : a < b ? -1 : 1;
-    });
+    if(a < b) {
+      return -1
+    }
+  });
 
   // Only change code above this line
 }
