0abdd0ec695954249c048c48ab174f1857c4f97d
caathyn <caathynguyen@gmail.com>
2022-06-23 17:16:37 -0700

    solved seekanddestory

--- seekAndDestory.js
+++ seekAndDestory.js
@@ -1,5 +1,14 @@
 function destroyer(arr) {
-  return arr;
+  const argsArray = Array.from(arguments).slice(1);
+  for(let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < argsArray.length; j++) {
+      if(arr[i] === argsArray[j]) {
+        delete arr[i]
+      }
+
+    }
+  }
+  return arr.filter(Boolean);
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
