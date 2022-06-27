5a3104eba6957fa728400b981f10bd1c6b71d925
caathyn <caathynguyen@gmail.com>
2022-06-27 05:52:54 -0700

    solved spinaltapcase

--- spinalTapCase.js
+++ spinalTapCase.js
@@ -1,5 +1,5 @@
 function spinalCase(str) {
-  return str;
+  return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+console.log(spinalCase('This Is Spinal Tap'));
