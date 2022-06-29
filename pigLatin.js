47db53ac31021c1ccae86e43a248ac76f62464f0
caathyn <caathynguyen@gmail.com>
2022-06-29 14:06:16 -0700

    solved piglatin

--- pigLatin.js
+++ pigLatin.js
@@ -1,5 +1,26 @@
-function translatePigLatin(str) {
-  return str;
+function isVowel(char) {
+  const vowels = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true
+   }
+   return !!vowels[char.toLowerCase()]
 }
 
-translatePigLatin("consonant");
+function translatePigLatin(word) {
+  let consonants = '';
+  if(isVowel(word[0])) {
+    return word + "way";
+  } else {
+    while(word && isVowel(word[0]) === false) {
+      consonants += word.slice(0,1);
+      word = word.slice(1);
+    }
+    word += consonants + "ay"
+  }
+  return word;
+}
+
+console.log(translatePigLatin("consonant"));
