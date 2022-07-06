2e0f2a0887626d4945a51710c9fc04cf5a350a10
caathyn <caathynguyen@gmail.com>
2022-07-06 12:13:37 -0700

    solved caesarscipher

---
+++ caesarsCipher.js
@@ -0,0 +1,18 @@
+function rot13(str) {
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    let letterIndex = alphabet.indexOf(str[i])
+    if (letterIndex >= 13) {
+      result += alphabet[letterIndex - 13]
+    } else if (letterIndex < 13 && letterIndex > -1) {
+      result += alphabet[letterIndex + 13]
+    } else {
+      result += str[i]; 
+    }
+
+  }
+  return result;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
