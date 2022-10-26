53e24fd6840473745e4d810db5187135c5c0b9f9
caathyn <caathynguyen@gmail.com>
2022-10-26 10:59:40 -0700

    solved largestnumber

--- largestNumber.js
+++ largestNumber.js
@@ -1,15 +1,19 @@
 function largestOfFour(arr) {
-  let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    let largeNum = arr[i][0];
-    for (let j = 0; j < arr[i].length; j++) {
-      if(arr[i][j] > largeNum) {
-        largeNum = arr[i][j]
-      }
+  let result = []; // [5, ]
+
+  for (let i = 0; i < arr.length; i++) { 
+    let largestNum = -Infinity;
+    const subArr = arr[i];
+
+    for (let j = 0; j < arr[i].length; j++) { 
+      largestNum = Math.max(subArr[j], largestNum);
     }
-    result[i] = largeNum;
+
+    result.push(largestNum);
   }
+  
   return result;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 2000]]));
+
